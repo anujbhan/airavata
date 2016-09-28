01ff91c370e5a7e8a05bdd9aaade15e1ce03e118
Anuj Bhandar <bhandar.anuj@gmail.com>
2016-09-28 16:43:14 -0400

    service structure ready, thrift-server implemented

---
+++ modules/resource-management/resource-management-service/src/main/java/org/apache/airavata/resource/management/UserGatewayPreferenceHandler.java
@@ -0,0 +1,7 @@
+package org.apache.airavata.resource.management;
+
+/**
+ * Created by abhandar on 9/28/16.
+ */
+public class UserGatewayPreferenceHandler {
+}
---
+++ modules/resource-management/resource-management-service/src/main/java/org/apache/airavata/resource/server/ResourceManagementServer.java
@@ -0,0 +1,7 @@
+package org.apache.airavata.resource.server;
+
+/**
+ * Created by abhandar on 9/28/16.
+ */
+public class ResourceManagementServer {
+}
